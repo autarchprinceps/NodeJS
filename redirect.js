428d10d39639e6b49b0216657c35ed08def755f1
autarchprinceps <autarch@live.de>
2013-02-08 22:48:11 +0100

    Version 3

---
+++ redirect.js
@@ -0,0 +1,18 @@
+var url = require('url');
+
+var redirect = function (domain) {
+    return function (req, res) {
+        var hostHeader = req.headers.host || '';
+        if (hostHeader.indexOf('localhost') === 0 || hostHeader.indexOf(domain) === 0) {
+            return;
+        }
+        var options = url.parse(req.url);
+        options.host = domain;
+        res.writeHead(301, {
+            'Location': url.format(options)
+        });
+        res.end();
+    };
+};
+
+module.exports = redirect;
--- silkveil.js
+++ silkveil.js
@@ -1,8 +1,10 @@
+var redirect = require('./redirect.js')('silkveiljs.com');
 var mappings = require('./mappings.js');
 var actions = require('./actions.js');
 var http = require('http');
 
 http.createServer(function (req, res) {
+    redirect(req, res);
     var alias = req.url.substring(1);
     var mapping = mappings[alias] || {
         action: 'error',
