441657fb45cbd734d241fe128b258861ce9fb1dd
autarchprinceps <autarch@live.de>
2013-02-22 22:20:18 +0100

    Socket.io finished

--- chat.socket.io.js
+++ chat.socket.io.js
@@ -8,9 +8,36 @@ app.get('/', function (req, res) {
     res.sendfile(__dirname + '/chat.html');
 });
 
+var sockets = [];
+var messages = [];
+
 io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+    socket.emit('put', {
+        Name: 'System',
+        Message: 'Welcome'
+    });
+    sockets.push(socket);
+    for (var j = 0; j < messages.length; j++) {
+        socket.emit('put', messages[j]);
+    }
+    socket.on('send', function (data) {
+        socket.set('nickname', data.Name);
+        messages.push(data);
+        for (var i = 0; i < sockets.length; i++) {
+            sockets[i].emit('put', data);
+        }
+    });
+    socket.on('disconnect', function () {
+        socket.get('nickname', function (err, name) {
+            var data = {
+                Name: 'System',
+                Message: name + " has left the chat"
+                }
+            messages.push(data);
+            for (var i = 0; i < sockets.length; i++) {
+                sockets[i].emit('put', data);
+            }
+        });
+        sockets.splice(sockets.indexOf(socket), 1);
     });
 });
